abe544f8794df74453d4386005f4dd1e630652ee
SMakela <73409799+smakela13@users.noreply.github.com>
2021-08-06 19:31:16 -0400

    Set up image upload route - Set up image upload functionality for front-end

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+import React, { useRef } from 'react';
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +7,8 @@ const ThoughtForm = () => {
     thought: "",
   });
   const [characterCount, setCharacterCount] = useState(0);
+
+  const fileInput = useRef(null);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -38,34 +41,65 @@ const ThoughtForm = () => {
     setCharacterCount(0);
   };
 
+  const handleImageUpload = (event) => {
+		event.preventDefault();
+		const data = new FormData();
+		data.append('image', fileInput.current.files[0]);
+
+		const postImage = async () => {
+			try {
+				const res = await fetch('/api/image-upload', {
+					mode: 'cors',
+					method: 'POST',
+					body: data,
+				});
+				if (!res.ok) throw new Error(res.statusText);
+				const postResponse = await res.json();
+				setFormState({ ...formState, image: postResponse.Location });
+				console.log('postImage: ', postResponse.Location);
+				return postResponse.Location;
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		postImage();
+  };
+
   return (
-    <div>
-      <p className={`m-0 ${characterCount === 280 ? "text-error" : ""}`}>
-        Character Count: {characterCount}/280
-      </p>
-      <form
-        className="flex-row justify-center justify-space-between-md align-stretch"
-        onSubmit={handleFormSubmit}
-      >
-        <input
-          placeholder="Name"
-          name="username"
-          value={formState.username}
-          className="form-input col-12 "
-          onChange={handleChange}
-        ></input>
-        <textarea
-          placeholder="Here's a new thought..."
-          name="thought"
-          value={formState.thought}
-          className="form-input col-12 "
-          onChange={handleChange}
-        ></textarea>
-        <button className="btn col-12 " type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+		<div>
+			<p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+				Character Count: {characterCount}/280
+			</p>
+			<form
+				className='flex-row justify-center justify-space-between-md align-stretch'
+				onSubmit={handleFormSubmit}>
+				<input
+					placeholder='Name'
+					name='username'
+					value={formState.username}
+					className='form-input col-12 '
+					onChange={handleChange}></input>
+				<textarea
+					placeholder="Here's a new thought..."
+					name='thought'
+					value={formState.thought}
+					className='form-input col-12 '
+					onChange={handleChange}></textarea>
+				<label className='form-input col-12  p-1'>
+					Add an image to your thought:
+					<input type='file' ref={fileInput} className='form-input p-2' />
+					<button
+						className='btn'
+						onClick={handleImageUpload}
+						type='submit'>
+						Upload
+					</button>
+				</label>
+				<button className='btn col-12 ' type='submit'>
+					Submit
+				</button>
+			</form>
+		</div>
   );
 };
 
